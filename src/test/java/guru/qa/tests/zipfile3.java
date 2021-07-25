e4da3141930d87443a34022126b2342b054e237b
suburbanknight89 <lumenteam@gmail.com>
2021-07-25 21:56:09 +0300

    add zip file 3

---
+++ src/test/java/guru/qa/tests/zipfile3.java
@@ -0,0 +1,43 @@
+package guru.qa.tests;
+
+import net.lingala.zip4j.exception.ZipException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static guru.qa.utils.Files.readTextFromPath;
+import static guru.qa.utils.Zip.unzip;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class zipfile3 {
+	@Test
+	void zipWithPasswordTest() throws IOException, ZipException {
+		String zipFilePath = "./src/test/resources/files/1.zip";
+		String unzipFolderPath = "./src/test/resources/files/unzip";
+		String password = "123";
+		String unzipTxtFilePath = "./src/test/resources/files/unzip/1.txt";
+		String expectedData = "Lorem ipsum dolor sit amet, consectetur adipiscing";
+		unzip(zipFilePath, unzipFolderPath, password);
+
+		String actualData = readTextFromPath(unzipTxtFilePath);
+
+		assertThat(actualData, containsString(expectedData));
+	}
+
+	@Test
+	void zipTest() throws IOException, ZipException {
+		String zipFilePath = "./src/test/resources/files/1.zip";
+		String unzipFolderPath = "./src/test/resources/files/unzip";
+		String unzipTxtFilePath = "./src/test/resources/files/unzip/1.txt";
+		String expectedData = "Lorem ipsum dolor sit amet, consectetur adipiscing";
+		String password = "123";
+
+		unzip(zipFilePath, unzipFolderPath, password);
+
+		String actualData = readTextFromPath(unzipTxtFilePath);
+
+		assertThat(actualData, containsString(expectedData));
+	}
+
+}
